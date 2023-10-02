8d6de048b8a7a9314c10027f0d7c399a614d8ef3
Bimal kafle <bimalkafle@bimals-MacBook-Pro.local>
2023-10-02 11:25:36 +0545

    videoUploadActivity design done

---
+++ app/src/main/java/np/com/bimalkafle/miniclip/VideoUploadActivity.kt
@@ -0,0 +1,11 @@
+package np.com.bimalkafle.miniclip
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VideoUploadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video_upload)
+    }
+}
