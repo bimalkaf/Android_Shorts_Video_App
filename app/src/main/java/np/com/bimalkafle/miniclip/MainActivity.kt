8c2fd9a1fbf5eae1b318ccf8785219705bb2cf03
Bimal kafle <bimalkafle@bimals-MacBook-Pro.local>
2023-10-02 11:11:03 +0545

    bottom bar designed

--- app/src/main/java/np/com/bimalkafle/miniclip/MainActivity.kt
+++ app/src/main/java/np/com/bimalkafle/miniclip/MainActivity.kt
@@ -2,10 +2,34 @@ package np.com.bimalkafle.miniclip
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import np.com.bimalkafle.miniclip.databinding.ActivityMainBinding
+import np.com.bimalkafle.miniclip.util.UiUtil
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavBar.setOnItemSelectedListener {menuItem->
+            when(menuItem.itemId){
+                R.id.bottom_menu_home->{
+                    UiUtil.showToast(this,"Home")
+                }
+                R.id.bottom_menu_add_video->{
+                    UiUtil.showToast(this,"Add video")
+                    //Goto VideoUploadActivity
+                }
+                R.id.bottom_menu_profile->{
+                    UiUtil.showToast(this,"Profile")
+                    //Goto ProfileActivity
+                }
+            }
+            false
+        }
+
     }
 }
